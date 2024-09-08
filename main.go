9aafbbe0afced641deaa67c3d4eed7c6bd33ca5f
Patryk <nevvgaming@gmail.com>
2024-09-08 02:19:23 +0200

    backend ngix

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
+)
 
 //TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
 
 func main() {
 	app := fiber.New()
-
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost", "http://eventpulse.local"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+	}))
 	app.Get("/", func(c fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
