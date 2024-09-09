3f07404190068ceff822837528a8d81a6ffcd1e6
Patryk <nevvgaming@gmail.com>
2024-09-09 02:23:18 +0200

    Backend base session for API

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/gofiber/fiber/v3/middleware/encryptcookie"
+	"github.com/gofiber/fiber/v3/middleware/session"
+	"strings"
+	"time"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+var store = session.New(session.Config{
+	CookieHTTPOnly: true,           // Ciasteczko HttpOnly - niedostępne dla JS
+	CookieSecure:   true,           // Ciasteczko dostępne tylko przez HTTPS
+	CookieSameSite: "Lax",          // Ochrona przed atakami CSRF
+	Expiration:     24 * time.Hour, // Czas ważności sesji
+})
 
 func main() {
 	app := fiber.New()
@@ -14,12 +23,53 @@ func main() {
 		AllowOrigins: []string{"http://localhost", "http://eventpulse.local"},
 		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
 	}))
-	app.Get("/", func(c fiber.Ctx) error {
+	app.Use(
+		encryptcookie.New(encryptcookie.Config{
+			Key: "",
+		}),
+		myMiddleware)
+	app.Get("api/", func(c fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+	app.Get("api/login/:name", func(c fiber.Ctx) error {
+		param := c.Params("name", "")
+		if param == "" {
+			return c.SendString("No name")
+		}
+		sess, err := store.Get(c)
+		if err != nil {
+			return err
+		}
+		state := sess.Get("userState")
 
+		if state != nil && state != param {
+			return c.SendString("Wrong user. Skipping")
+		}
+
+		if state != nil {
+			return c.SendString("Logged")
+		}
+		sess.Set("userState", param)
+		sess.Save()
+		return c.SendString("Logged")
+	})
 	app.Listen(":8080")
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func myMiddleware(c fiber.Ctx) error {
+	println("Middleware: Żądanie otrzymane")
+
+	if strings.Contains(c.OriginalURL(), "/api/login") {
+		return c.Next()
+	}
+
+	sess, _ := store.Get(c)
+	state := sess.Get("userState")
+
+	if state == nil {
+		return c.SendString("No logged user")
+	}
+	println("Hello, World! Witaj: " + fmt.Sprint(sess.Get("userState")))
+
+	return c.Next()
+}
